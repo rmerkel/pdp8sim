665b67752d3f233bc99e45c7e1cf3ed9cbc9907e
Randy T. Merkel <rmerkel@acm.org>
2024-07-21 23:41:49 -0700

    add_chk_ovr.pa

--- README.md
+++ README.md
@@ -1,31 +1,39 @@
-## pdp8sim
-A PDP-8 Simulator, v0.0
+## pdp8sim - a PDP-8 Simulator
+
+A PDP-8 simulator, just for the sake of it.
+Version 0.1, pre-release.
+
+Current status:
+
+* IOT is not supported
+* OPR Group 1 and 2 under test
+* Can load BIN files from the command line.
 
 ## PDP-8/I Major State Flow Diagram
 
-	   |<---- 1 memory cycle ----->|<---- 1 memory cycle ---->|<---- 1 memory cycle ---->|
-	
-	     MRI Direct/Indirect
-	   |<--------------------------------------------------------------------------+
-	   | JMP Indirect                                                              |
-	   |<---------------------------------------------+                            |
-	   | JMP Direct                                   |                            |
-	   |<----------------_+                           |                            |
-	   \       +-------+  |                +-------+  |               +---------+  |
-	    \      |       |--+  MRI Indirect  |       |--+               |         |  |
-	+--->----->| Fetch |------------------>| Defer |----------------->| Execute |--+
-	|   /      |       |----------------+  |       |                  |         |
-	|  /       +-------+--+             |  +-------+              +-->+---------+
-	|  |  IOT & Operate   |             |        MRI Direct       |
-	|  +------------------+             +-------------------------+
-	|
-	| |<---- MD = Instruction --->|<-----_ MD = Address ----->|<---- MD = Operand  ---->|
-	|
-	|                                      +--------+
-	|     Start up and Manual Operation -->| Direct |
-	|                                      | Memory |----+
-	|                   Data Break ------->| Access |    |
-	|                                      +--------+    |
-	|             Start up & Man Op Only                 |
-	+----------------------------------------------------+
+      |<---- 1 memory cycle ----->|<---- 1 memory cycle ---->|<---- 1 memory cycle ---->|
+    
+         MRI Direct/Indirect
+       |<--------------------------------------------------------------------------+
+       | JMP Indirect                                                              |
+       |<---------------------------------------------+                            |
+       | JMP Direct                                   |                            |
+       |<----------------_+                           |                            |
+       \       +-------+  |                +-------+  |               +---------+  |
+        \      |       |--+  MRI Indirect  |       |--+               |         |  |
+    +--->----->| Fetch |------------------>| Defer |----------------->| Execute |--+
+    |   /      |       |----------------+  |       |                  |         |
+    |  /       +-------+--+             |  +-------+              +-->+---------+
+    |  |  IOT & Operate   |             |        MRI Direct       |
+    |  +------------------+             +-------------------------+
+    |
+    | |<---- MD = Instruction --->|<-----_ MD = Address ----->|<---- MD = Operand  ---->|
+    |
+    |                                      +--------+
+    |     Start up and Manual Operation -->| Direct |
+    |                                      | Memory |----+
+    |                   Data Break ------->| Access |    |
+    |                                      +--------+    |
+    |             Start up & Man Op Only                 |
+    +----------------------------------------------------+
 
--- pdp8sim.cc
+++ pdp8sim.cc
@@ -74,62 +74,66 @@ static const char* toString(const State state) {
  * Constants
  ************************************************************************************************/
 
-const unsigned UINT12_MAX	= 07777;
-const int	   INT12_MAX	= +2047;
-const int	   INT12_MIN	= -2048;
+const unsigned UINT12_MAX		= 07777;
+const int	   INT12_MAX		= +2047;
+const int	   INT12_MIN		= -2048;
 
 /************************************************************************************************
  * Bit maskes
  ************************************************************************************************/
 
-const unsigned Page_Mask    = 07600;		///< PC Address Page address mask
-const unsigned Op_Mask		= 07000;		///< OpCode mask
-const unsigned Op_Shift		= 9;			///< OpCode shift
-const unsigned I_Mask		= 00400;		///< Indirect bit
-const unsigned P_Mask		= 00200;		///< Page bit
-const unsigned Addr_Mask	= 00177;		///< Address/page offset mask
+const unsigned Page_Mask    	= 07600;	///< PC Address Page address mask
+const unsigned Op_Mask			= 07000;	///< OpCode mask
+const unsigned Op_Shift			= 9;		///< OpCode shift
+const unsigned I_Mask			= 00400;	///< Indirect bit
+const unsigned P_Mask			= 00200;	///< Page bit
+const unsigned Addr_Mask		= 00177;	///< Address/page offset mask
+
+const unsigned Sign_Mask		= 04000;	///< 2's complement sign mask
 
 // OPER Group 1
 
-const unsigned GROUP1		= 00400;		///< Bit 3 is clear
+const unsigned GROUP1			= 00400;	///< Bit 3 is clear
 
-const unsigned GRP1_NOP		= 07000;		///< NOP
-const unsigned GRP1_CLA		= 07200;		///< Clear AC,	sequence 1
-const unsigned GRP1_CLL		= 07100;		///< Clear link, sequence 1
-const unsigned GRP1_CMA		= 07040;		///< Complement AC, sequence 2
-const unsigned GRP1_CML		= 07020;		///< Complement link, sequence 2
-const unsigned GRP1_RAR		= 07010;		///< Rotate AC and L right 1, sequence 4
-const unsigned GRP1_RTR		= 07012;		///< Rotate AC and L right 2, sequence 4
-const unsigned GRP1_RTL		= 07006;		///< Rotate AC and L left 1, sequence 4
-const unsigned GRP1_RAL		= 07004;		///< Rotate AC and L left 2, sequence 4
-const unsigned GRP1_IAC		= 07001;		///< Increment AC, sequence 3
+const unsigned GRP1_NOP			= 07000;	///< NOP
+const unsigned GRP1_CLA			= 07200;	///< Clear AC,	sequence 1
+const unsigned GRP1_CLL			= 07100;	///< Clear link, sequence 1
+const unsigned GRP1_CMA			= 07040;	///< Complement AC, sequence 2
+const unsigned GRP1_CML			= 07020;	///< Complement link, sequence 2
+const unsigned GRP1_RAR			= 07010;	///< Rotate AC and L right 1, sequence 4
+const unsigned GRP1_RTR			= 07012;	///< Rotate AC and L right 2, sequence 4
+const unsigned GRP1_RTL			= 07006;	///< Rotate AC and L left 1, sequence 4
+const unsigned GRP1_RAL			= 07004;	///< Rotate AC and L left 2, sequence 4
+const unsigned GRP1_IAC			= 07001;	///< Increment AC, sequence 3
 
 // OPER Group 2
 
-const unsigned GROUP2		= 00400;		///< Bit 3 is set, bit 11 is clear.
+const unsigned GROUP2			= 00400;	///< Bit 3 is set, bit 11 is clear.
 
-const unsigned	GRP2_SMA	= 07500;		///< Skip on minus AC, sequence 1
-const unsigned	GRP2_SZA	= 07440;		///< Skip on zero AC, sequence 1
-const unsigned	GRP2_SPA	= 07510;		///< Skip on plus AC, sequence 1
-const unsigned 	GRP2_SNA	= 07450;		///< Skip on plus AC, sequence 1
-const unsigned	GRP2_SNL	= 07420;		///< Skip on non-zero AC, sequence 1
-const unsigned	GRP2_SZL	= 07430;		///< Skip on zero link, sequence 1
-const unsigned	GRP2_SKP	= 07410;		///< Skip unconditionally, sequence 1
-const unsigned	GRP2_OSR	= 07404;		///< Inclusive OR, switch register with AC, sequence 3
-const unsigned	GRP2_HLT	= 07402;		///< Halt the processor, sequence 3
-const unsigned	GRP2_CLA	= 07600;		///< Clear AC, sequence 2
+const unsigned	GRP2_SKP_BIT	= 00010;	///< Skip bit for bits 5-7
+
+const unsigned	GRP2_SMA		= 07500;	///< Skip on minus AC, sequence 1
+const unsigned	GRP2_SZA		= 07440;	///< Skip on zero AC, sequence 1
+const unsigned	GRP2_SPA		= 07510;	///< Skip on plus AC, sequence 1
+const unsigned 	GRP2_SNA		= 07450;	///< Skip on plus AC, sequence 1
+const unsigned	GRP2_SNL		= 07420;	///< Skip on non-zero AC, sequence 1
+const unsigned	GRP2_SZL		= 07430;	///< Skip on zero link, sequence 1
+const unsigned	GRP2_SKP		= 07410;	///< Skip unconditionally, sequence 1
+const unsigned	GRP2_OSR		= 07404;	///< Inclusive OR, switch register with AC, sequence 3
+const unsigned	GRP2_HLT		= 07402;	///< Halt the processor, sequence 3
+const unsigned	GRP2_CLA		= 07600;	///< Clear AC, sequence 2
 
 
 /********************************************************************************************//**
  * PDP8 Registers
  ************************************************************************************************/
 struct Registers {
-    uint16_t    pc      : 12;       // Program Counter - may expand to include df and if
-    uint16_t    ac      : 12;		// ACcumulator register
-    uint16_t     l      :  1;		// Link register
-    uint16_t    ma      : 12;       // Memory address register
-    uint16_t    md      : 12;		// Memory data register
-	uint16_t	sr		: 12;		// Switch register
+    uint16_t    pc      : 12;       		///< Program Counter - may expand to include df and if
+    uint16_t    ac      : 12;				///< ACcumulator register
+    uint16_t     l      :  1;				///< Link register
+    uint16_t    ma      : 12;       		///< Memory address register
+    uint16_t    md      : 12;				///< Memory data register
+	uint16_t	sr		: 12;				///< Switch register
 	OpCode		ir;
 
     Registers() : pc{0}, ac{0}, l{0}, ma{0}, md{0}, sr{0}, ir{OpCode::AND}  {}
@@ -139,11 +143,11 @@ struct Registers {
  * Decoded instructon
  ************************************************************************************************/
 struct Decoded {
-	OpCode		op;					///< Opcode
-	bool		i;					///< Indirect?
-	bool		p;					///< Current page?
-	uint16_t	eaddr;				///< Effective addr
-	uint16_t	bits;				///< bits 3-11 of the instruction (for OPR, IOT, tbd)
+	OpCode		op;							///< Opcode
+	bool		i;							///< Indirect?
+	bool		p;							///< Current page?
+	uint16_t	eaddr;						///< Effective addr
+	uint16_t	bits;						///< bits 3-11 of the instruction (for OPR, IOT, tbd)
 };
 
 /********************************************************************************************//**
@@ -237,42 +241,56 @@ static void oper_group1(unsigned instrs) {
 static void oper_group2(unsigned instrs) {
 	// Sequence 1
 
-	if ((instrs & GRP2_SMA) == GRP2_SMA)		
-		if (r.ac < 0)	// NO,r.ac is an unsigned, check bit 0 (PDP-8 bit order)
+	if ((instrs & GRP2_SKP_BIT) == GRP2_SKP_BIT) {	// SKP bit is 1
+		bool condition = false;						// true for unconditional SKP
+
+		if ((instrs & GRP2_SPA) == GRP2_SPA) {
+			condition = true;
+			if ((r.ac & Sign_Mask) != Sign_Mask)
+				++r.pc;
+		}
+
+		if ((instrs & GRP2_SNA) == GRP2_SNA) {
+			condition = true;
+			if (r.ac != 0)
+				++r.pc;
+		}
+
+		if ((instrs & GRP2_SZL) == GRP2_SZL) {
+			condition = true;
+			if (r.l == 0)
+				++r.pc;
+		}
+
+		if ((instrs & GRP2_SKP) == GRP2_SKP && !condition)
 			++r.pc;
 
-	if ((instrs & GRP2_SZA) == GRP2_SZA)
-		assert(false);
+	} else {							// SKP bit is 0
+		if ((instrs & GRP2_SMA) == GRP2_SMA)
+			if ((r.ac & Sign_Mask) == Sign_Mask)
+				++r.pc;
 
+		if ((instrs & GRP2_SZA) == GRP2_SZA)
+			if (r.ac == 0)
+				++r.pc;
+
+		if ((instrs & GRP2_SNL) == GRP2_SNL)
+			if (r.l != 0)
+				++r.pc;
+	}
+	
 	// Sequence 2
 
-	if ((instrs & GRP2_SPA) == GRP2_SPA)
-		assert(false);
-
-	if ((instrs & GRP2_SNA) == GRP2_SNA)
-		assert(false);
+	if ((instrs & GRP2_CLA) == GRP2_CLA)
+		r.ac = 0;
 
 	// Sequence 3
 
-	if ((instrs & GRP2_SNL) == GRP2_SNL)
-		assert(false);
-
-	// Sequence 4
-
-	if ((instrs & GRP2_SZL) == GRP2_SZL)
-		assert(false);
-
-	if ((instrs & GRP2_SKP) == GRP2_SKP)
-		assert(false);
-
 	if ((instrs & GRP2_OSR) == GRP2_OSR)
-		assert(false);
+		r.ac |= r.sr;
 
 	if ((instrs & GRP2_HLT) == GRP2_HLT)
 		run = false;
-
-	if ((instrs & GRP2_CLA) == GRP2_CLA)
-		assert(false);
 }
 
 /********************************************************************************************//**
@@ -286,42 +304,53 @@ static void oper(unsigned instrs) {
 		oper_group2(instrs);
 
 	else
-		assert(false);						// Other group are not implenentated
+		assert(false);						// Other groups are not implenentated
+}
+
+/********************************************************************************************//**
+ * Decode a instruction
+ ************************************************************************************************/
+static Decoded decode(unsigned instr) {
+	Decoded d;
+	d.op		= static_cast<OpCode>((instr & Op_Mask)	>> Op_Shift);
+	d.i		= (instr & I_Mask) == I_Mask;
+	d.p		= (instr & P_Mask) == P_Mask;
+	unsigned	addr	= instr & Addr_Mask;
+
+	d.eaddr	= d.p ? r.pc & Page_Mask : 0;
+	d.eaddr |= addr;
+	d.bits = instr & 00777;
+
+	return d;
 }
 
 /********************************************************************************************//**
  * Fetch next instruction, handle JMP direct
  ************************************************************************************************/
 void fetch() {
-	r.md 					= mem[r.pc++];	// decode
-	r.ir 					= static_cast<OpCode>((r.md & Op_Mask)	>> Op_Shift);
-	const bool		i		= (r.md & I_Mask) == I_Mask;
-	const bool		p		= (r.md & P_Mask) == P_Mask;
-	const unsigned	addr	= r.md & Addr_Mask;
-
 	++ninstrs;
-	r.ma = p ? r.pc & Page_Mask : 0;
-	r.ma |= addr;
+
+	r.md 			= mem[r.pc++];
+	const Decoded d	= decode(r.md);
+	r.ir 			= d.op;
+	r.ma 			= d.eaddr;
 
     if (r.ir == OpCode::IOT) {			// IOT?
 		assert(false);					// ... not implemented!
         s = State::Fetch;
 
-	}
-	else if (r.ir == OpCode::OPR) {	// OPR?
+	} else if (r.ir == OpCode::OPR) {		// OPR?
 		oper(r.md);
 		s = State::Fetch;
 
-    }
-	else if (i)                  		// Indirect?
+    } else if (d.i)                  		// Indirect?
        s = State::Defer;				//	r.ma is the address of the operation
 	   
     else if (r.ir == OpCode::JMP) {		// JMP direct?
         r.pc = r.ma;
         s = State::Fetch;
 
-    }
-	else
+    } else
        s = State::Execute;
 }
 
@@ -330,9 +359,8 @@ void fetch() {
  ************************************************************************************************/
 void defer() {
 	r.md = mem[r.ma];					// Fetch indirect operand
-	if (r.ma >= 010 && r.ma <= 017) {
+	if (r.ma >= 010 && r.ma <= 017)
 		mem[r.ma] = ++r.md;				// Auto increment
-	}
 
 	if (r.ir == OpCode::JMP) {			// JMP indirect?
 		r.pc = r.md;
@@ -357,7 +385,7 @@ void execute() {
 		uint16_t sum = r.ac + r.md;
 		if (sum & UINT12_MAX)
 			r.l = ~r.l;
-		r.ac = sum & 07777;
+		r.ac = sum & UINT12_MAX;
 	} break;
 			
 	case OpCode::ISZ:
@@ -396,47 +424,48 @@ void execute() {
  * Break (DMA) state
  ************************************************************************************************/
 void brk() {
+	assert(false);			// Not implemented
     s = State::Fetch;
 }
 
 /********************************************************************************************//**
- * Dump the processor state
- *
- * @note	The state is the current state, after the previous instruction, but before the 
- *			current!
- *
- * @param	compact	true for compact display
+ * disasmble the the next instruction
  ************************************************************************************************/
-static void dump(bool compact = false) {
-	cout	<< ninstrs << " instrs "	<< ncycles << " cycles " << '(' << ncycles * 1.5 << "us)\n";
+static void disasm(unsigned addr, unsigned instr) {
+	addr	&= UINT12_MAX;
+	instr	&= UINT12_MAX;
+	const Decoded d = decode(instr);
 
-	if (compact) {
-		cout	<< 	toString(r.ir) << ' ' << toString(s);
-		if (sw.sinstr)
-			cout << " SInstr";
-		if (sw.sstep)
-			cout << " SStep";
-    	cout	<< 				   " PC "	<< oct << setfill('0') << setw(4) << r.pc 
-    			<< 				   " MA "	<< oct << setfill('0') << setw(4) << r.ma  << ' '
-            	<< 				   " MD "	<< oct << setfill('0') << setw(4) << r.md  << ' '
-				<< "L " << r.l	<< " AC "	<< oct << setfill('0') << setw(4) << r.ac  << ' ' 
-    			<< 				   " SR "	<< oct << setfill('0') << setw(4) << r.sr  << '\n';
+	cout << oct	<< setfill('0')	<< setw(4) 	<< addr << ' ' << toString(d.op) << ' ';
+	switch (d.op) {
+		case OpCode::OPR:
+		case OpCode::IOT:
+			cout << oct	<< setfill('0')	<< setw(3) 	<< d.bits;
+			break;
 
-	} else {
-
-    	cout	<< 				   "    PC " << oct << setfill('0') << setw(4) << r.pc	<< ' '
-				<< 	toString(r.ir) << ' ' << toString(s);
-		if (sw.sinstr)
-			cout << " SInstr";
-		if (sw.sstep)
-			cout << " SStep";
-		cout	<< '\n';
-
-    	cout	<< 				   "    MA " << oct << setfill('0') << setw(4) << r.ma  << '\n'
-            	<< 				   "    MD " << oct << setfill('0') << setw(4) << r.md  << '\n'
-				<< "L " << r.l	<<    " AC " << oct << setfill('0') << setw(4) << r.ac  << '\n' 
-    			<< 				   "    SR " << oct << setfill('0') << setw(4) << r.sr  << '\n';
+		default:
+			if (d.i)
+				cout << "I ";
+			cout  << oct << setfill('0') << setw(4) << d.eaddr;
 	}
+}
+
+/********************************************************************************************//**
+ * Dump the processor state
+ ************************************************************************************************/
+static void dump() {
+    cout	<< 				   "PC " << oct << setfill('0') << setw(4) << r.pc	<< ' '
+			<< "L " << r.l	<< ' '
+			<<				   "AC " << oct << setfill('0') << setw(4) << r.ac  << '\n' 
+    		<< 				   "MA " << oct << setfill('0') << setw(4) << r.ma  << ' '
+           	<< 			   "    MD " << oct << setfill('0') << setw(4) << r.md  << '\n';
+
+	cout	<< 				   "IR " << toString(r.ir)	<< ' '
+			<< 			   "     S " << toString(s)		<< '\n';
+
+	disasm(r.pc, mem[r.pc]);
+	cout	<< "    /"
+			<< ninstrs << " instrs "	<< ncycles << " cycles " << '(' << ncycles * 1.5 << "us)\n";
 }
 
 /********************************************************************************************//**
