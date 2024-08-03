693a22fab35fd491d9b75e8ba8068a7f3c1f5974
Randy T. Merkel <rmerkel@acm.org>
2024-08-02 20:23:50 -0700

    bump version

--- pdp8sim.cc
+++ pdp8sim.cc
@@ -695,7 +695,7 @@ int main (int argc, char** argv) {
 
 				switch(c) {
 					case '?': case 'h': help();			return 1;
-					case 'v': cout << "version 0.4\n";	return 1;
+					case 'v': cout << "version 0.5\n";	return 1;
 					default:
 						cerr << progName << ": unknown option '" << c << "'.\n";
 						return 1;
